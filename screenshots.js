abca6e6fe3589cd45d8c9b15ecfd3feeafcad6c8
wasabina67 <wasabina67@gmail.com>
2024-09-08 12:33:27 +0900

    Update screenshots.js

--- screenshots.js
+++ screenshots.js
@@ -1,25 +1,30 @@
 const puppeteer = require('puppeteer');
-const url = 'https://garage-mash.com/';
+const urls = ['https://garage-mash.com/'];
 
 (async () => {
   const browser = await puppeteer.launch({ headless: 'new' });
   const page = await browser.newPage();
 
-  await page.goto(url);
-  await page.screenshot({ path: 'screenshots/default.png' });
-
-  const devices = [
-    { name: 'iPhone SE', fileName: 'iphone-se.png' },
-    { name: 'iPhone 13 Pro', fileName: 'iphone-13-pro.png' },
-    { name: 'iPad Pro 11', fileName: 'ipad-pro-11.png' },
-  ];
-  for (const device of devices) {
-    const dev = puppeteer.KnownDevices[device.name];
-    await page.emulate(dev);
+  let index = 0;
+  for (const url of urls) {
     await page.goto(url);
-    await page.screenshot(
-      { path: 'screenshots/' + device.fileName, fullPage: true }
-    );
+    await page.screenshot({ path: 'screenshots/default.png' });
+
+    const devices = [
+      { name: 'iPhone SE', fileName: index + '_iphone-se.png' },
+      { name: 'iPhone 13 Pro', fileName: index + '_iphone-13-pro.png' },
+      { name: 'iPad Pro 11', fileName: index + '_ipad-pro-11.png' },
+    ];
+    for (const device of devices) {
+      const dev = puppeteer.KnownDevices[device.name];
+      await page.emulate(dev);
+      await page.goto(url);
+      await page.screenshot(
+        { path: 'screenshots/' + device.fileName, fullPage: true }
+      );
+    }
+
+    index = index + 1
   }
 
   await browser.close();
